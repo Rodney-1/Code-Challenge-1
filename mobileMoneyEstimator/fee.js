6b3d3a53a09fcd9f34e4e2a88f790cfbb509d7b0
Rodney_Karani <rodneykarani07@gmail.com>
2025-06-08 20:21:35 +0300

    Make changes to mobileMoneyEstimator

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -10,4 +10,20 @@ if (isNaN(amountToSend) || amountToSend <= 0) {
 if (amountToSend <=0) {
     console.error("Input should be more than 0");
     return;
+};
+let transactionFee = amountToSend * 0.015;
+//Conditions
+if(transactionFee < 10) {
+    transactionFee = 10;
+} else if (transactionFee > 70) {
+    transactionFee = 70;
 }
+transactionFee = Math.round(transactionFee * 100) / 100;
+const totalAmount = amountToSend + transactionFee;
+console.log(`Sending KES ${amountToSend}:`);
+console.log(`Transaction fee: KES ${transactionFee}`);
+console.log(`Total amount to be debited: KES ${totalAmount}`);
+console.log("Send Money Securely!");
+}
+
+calculateFee();
